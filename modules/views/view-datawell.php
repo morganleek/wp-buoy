e90357857acd275f69eed4990dd03b3110566972
morganleek <morganleek@gmail.com>
2021-04-07 05:56:29 +0000

    hide new charts

--- modules/views/view-datawell.php
+++ modules/views/view-datawell.php
@@ -78,15 +78,15 @@
 						$wave_from = $recent->timestamp;
 						$wave_until = date('Y-m-d H:i:s', strtotime('-3 days', strtotime($recent->timestamp))); // 216000sec is 2.5 days
 
-						$html .= '<div class="chart-js-layout chart-js-layout-' . $b->buoy_id . '" data-buoy="' . $b->buoy_id . '" data-buoy-type="datawell" style="width: 100%;">
-							<div class="chart-js-menu">
-								<button class="map-focus" aria-label="Map Focus"></button>
-								<button class="show-chart" aria-label="Show Chart"></button>
-								<button class="calendar-trigger" aria-label="Calendar Trigger"></button>
-							</div>
-							<p class="loading" style="text-align: center;">Loading&hellip;</p>
-							<canvas id="canvas-' . $b->buoy_id . '"></canvas>
-						</div>';
+						// $html .= '<div class="chart-js-layout chart-js-layout-' . $b->buoy_id . '" data-buoy="' . $b->buoy_id . '" data-buoy-type="datawell" style="width: 100%;">
+						// 	<div class="chart-js-menu">
+						// 		<button class="map-focus" aria-label="Map Focus"></button>
+						// 		<button class="show-chart" aria-label="Show Chart"></button>
+						// 		<button class="calendar-trigger" aria-label="Calendar Trigger"></button>
+						// 	</div>
+						// 	<p class="loading" style="text-align: center;">Loading&hellip;</p>
+						// 	<canvas id="canvas-' . $b->buoy_id . '"></canvas>
+						// </div>';
 
 						// Fetch Wave Points
 						$args = uwa_datawell_wave_points($b, $wave_from, $wave_until, $uwa_datawell_time_adjustment);
